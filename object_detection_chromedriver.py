e289772242e0339b8b88ee332e3a35213cf03472
Jake <imjakeeyy@gmail.com>
2020-01-09 18:16:36 +0100

    First project in python, using chromedriver and openCV

---
+++ object_detection_chromedriver.py
@@ -0,0 +1,91 @@
+from cv2 import cv2
+import numpy as np
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import wait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import TimeoutException
+
+def my_detection(outs):
+    print("IN detectiopn")
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    font = cv2.FONT_HERSHEY_PLAIN
+    cars_parked = 0
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            if label == "car":
+                cars_parked += 1
+
+            color = colors[i]
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(img, label, (x, y + 30), font, 3, color, 3)
+    
+    print("cars parked right now " + str(cars_parked))
+       
+    
+
+browser = webdriver.Chrome()
+browser.set_window_size(1600, 900)
+browser.get('http://128.197.128.165/view/viewer_index.shtml?id=2770')
+#fastrack = WebDriverWait(browser, 10).until(ec.visibility_of_element_located((By.XPATH, "/html/body/div[1]/div[3]/main/article/nav/ul/li[1]/a")));
+#fastrack.click()
+
+
+browser.save_screenshot("parking1.png")
+
+# Load Yolo
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+# Loading image
+while True:
+    browser.save_screenshot("parking1.png")
+    img = cv2.imread("parking1.png")
+    img = cv2.resize(img, None, fx=1, fy=1)
+    height, width, channels = img.shape
+
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # Showing informations on the screen
+    my_detection(outs)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+    #cv2.destroyWindow("Image")
